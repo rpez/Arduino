c2e7ba74cce8047ee7e397a67be773d328e75029
Reetu Kontio <reetu.kontio@gmail.com>
2020-01-21 09:32:42 +0200

    text scroller

--- led_test/led_test.ino
+++ led_test/led_test.ino
@@ -2,15 +2,6 @@
 
 FASTLED_USING_NAMESPACE
 
-// FastLED "100-lines-of-code" demo reel, showing just a few 
-// of the kinds of animation patterns you can quickly and easily 
-// compose using FastLED.  
-//
-// This example also shows one easy way to define multiple 
-// animations patterns and have them automatically rotate.
-//
-// -Mark Kriegsman, December 2014
-
 #if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
 #warning "Requires FastLED 3.1 or later; check github for latest code."
 #endif
@@ -30,96 +21,154 @@ void setup() {
   
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
 }
 
-
-// List of patterns to cycle through.  Each is defined as a separate function below.
-typedef void (*SimplePatternList[])();
-SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
-
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+int offset = 13;
+int column = 13;
   
 void loop()
 {
-  // Call the current pattern function once, updating the 'leds' array
-  gPatterns[gCurrentPatternNumber]();
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Black;
+  }
+  displayLetter('s', offset, 255, 0, 0);
+  displayLetter('e', offset + 7, 255, 255, 0);
+  displayLetter('n', offset + 14, 255, 0, 255);
+  displayLetter('d', offset + 20, 0, 255, 255);
 
+  displayLetter('n', offset + 34, 0, 0, 255);
+  displayLetter('u', offset + 40, 255, 255, 0);
+  displayLetter('d', offset + 47, 255, 255, 255);
+  displayLetter('e', offset + 54, 0, 255, 0);
+  displayLetter('s', offset + 61, 0, 255, 255);
+  
   // send the 'leds' array out to the actual LED strip
   FastLED.show();  
   // insert a delay to keep the framerate modest
   FastLED.delay(1000/FRAMES_PER_SECOND); 
 
   // do some periodic updates
-  EVERY_N_MILLISECONDS( 1 ) { gHue += 3; } // slowly cycle the "base color" through the rainbow
-  //EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+  EVERY_N_MILLISECONDS( 1 ) { gHue += 3; }
+  EVERY_N_MILLISECONDS( 75 ) {
+    if (offset < -80) {
+      offset = 14;
+    }
+    offset -= 1;
+  }
+}
+
+void displayLetter(char letter, int offset, int r, int g, int b) {
+  switch(letter) {
+    case 'e':
+      pixel(offset + 0, 0, r, g, b);
+      pixel(offset + 1, 0, r, g, b);
+      pixel(offset + 2, 0, r, g, b);
+      pixel(offset + 3, 0, r, g, b);
+      pixel(offset + 4, 0, r, g, b);
+      pixel(offset + 5, 0, r, g, b);
+      pixel(offset + 0, 1, r, g, b);
+      pixel(offset + 0, 2, r, g, b);
+      pixel(offset + 1, 2, r, g, b);
+      pixel(offset + 2, 2, r, g, b);
+      pixel(offset + 3, 2, r, g, b);
+      pixel(offset + 4, 2, r, g, b);
+      pixel(offset + 5, 2, r, g, b);
+      pixel(offset + 0, 3, r, g, b);
+      pixel(offset + 0, 4, r, g, b);
+      pixel(offset + 1, 4, r, g, b);
+      pixel(offset + 2, 4, r, g, b);
+      pixel(offset + 3, 4, r, g, b);
+      pixel(offset + 4, 4, r, g, b);
+      pixel(offset + 5, 4, r, g, b);
+      break;
+    case 'd':
+      pixel(offset + 1, 0, r, g, b);
+      pixel(offset + 2, 0, r, g, b);
+      pixel(offset + 3, 0, r, g, b);
+      pixel(offset + 4, 0, r, g, b);
+      pixel(offset + 0, 0, r, g, b);
+      pixel(offset + 0, 1, r, g, b);
+      pixel(offset + 0, 2, r, g, b);
+      pixel(offset + 0, 3, r, g, b);
+      pixel(offset + 0, 4, r, g, b);
+      pixel(offset + 1, 4, r, g, b);
+      pixel(offset + 2, 4, r, g, b);
+      pixel(offset + 3, 4, r, g, b);
+      pixel(offset + 4, 4, r, g, b);
+      pixel(offset + 5, 1, r, g, b);
+      pixel(offset + 5, 2, r, g, b);
+      pixel(offset + 5, 3, r, g, b);
+      break;
+    case 'n':
+      pixel(offset + 0, 0, r, g, b);
+      pixel(offset + 0, 1, r, g, b);
+      pixel(offset + 0, 2, r, g, b);
+      pixel(offset + 0, 3, r, g, b);
+      pixel(offset + 0, 4, r, g, b);
+      pixel(offset + 1, 1, r, g, b);
+      pixel(offset + 2, 2, r, g, b);
+      pixel(offset + 3, 3, r, g, b);
+      pixel(offset + 4, 0, r, g, b);
+      pixel(offset + 4, 1, r, g, b);
+      pixel(offset + 4, 2, r, g, b);
+      pixel(offset + 4, 3, r, g, b);
+      pixel(offset + 4, 4, r, g, b);
+      break;
+    case 's':
+      pixel(offset + 0, 0, r, g, b);
+      pixel(offset + 1, 0, r, g, b);
+      pixel(offset + 2, 0, r, g, b);
+      pixel(offset + 3, 0, r, g, b);
+      pixel(offset + 4, 0, r, g, b);
+      pixel(offset + 5, 0, r, g, b);
+      pixel(offset + 0, 1, r, g, b);
+      pixel(offset + 0, 2, r, g, b);
+      pixel(offset + 1, 2, r, g, b);
+      pixel(offset + 2, 2, r, g, b);
+      pixel(offset + 3, 2, r, g, b);
+      pixel(offset + 4, 2, r, g, b);
+      pixel(offset + 5, 2, r, g, b);
+      pixel(offset + 5, 3, r, g, b);
+      pixel(offset + 0, 4, r, g, b);
+      pixel(offset + 1, 4, r, g, b);
+      pixel(offset + 2, 4, r, g, b);
+      pixel(offset + 3, 4, r, g, b);
+      pixel(offset + 4, 4, r, g, b);
+      pixel(offset + 5, 4, r, g, b);
+      break;
+    case 'u':
+      pixel(offset + 0, 0, r, g, b);
+      pixel(offset + 0, 1, r, g, b);
+      pixel(offset + 0, 2, r, g, b);
+      pixel(offset + 0, 3, r, g, b);
+      pixel(offset + 1, 4, r, g, b);
+      pixel(offset + 2, 4, r, g, b);
+      pixel(offset + 3, 4, r, g, b);
+      pixel(offset + 4, 4, r, g, b);
+      pixel(offset + 5, 0, r, g, b);
+      pixel(offset + 5, 1, r, g, b);
+      pixel(offset + 5, 2, r, g, b);
+      pixel(offset + 5, 3, r, g, b);
+      break;
+    default:
+      break;
+  }
+}
+
+void pixel(int x, int y, int r, int g, int b) {
+  if (x < 13 && x >= 0) {
+    leds[x + y * column] = CRGB(r, g, b);
+  }
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
-
-void nextPattern()
-{
-  // add one to the current pattern number, and wrap around at the end
-  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
-}
 
 void rainbow() 
 {
   // FastLED's built-in rainbow generator
   fill_rainbow( leds, NUM_LEDS, gHue, 1);
-}
-
-void rainbowWithGlitter() 
-{
-  // built-in FastLED rainbow, plus some random sparkly glitter
-  rainbow();
-  addGlitter(80);
-}
-
-void addGlitter( fract8 chanceOfGlitter) 
-{
-  if( random8() < chanceOfGlitter) {
-    leds[ random16(NUM_LEDS) ] += CRGB::White;
-  }
-}
-
-void confetti() 
-{
-  // random colored speckles that blink in and fade smoothly
-  fadeToBlackBy( leds, NUM_LEDS, 10);
-  int pos = random16(NUM_LEDS);
-  leds[pos] += CHSV( gHue + random8(64), 200, 255);
-}
-
-void sinelon()
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
-  leds[pos] += CHSV( gHue, 255, 192);
-}
-
-void bpm()
-{
-  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
-  for( int i = 0; i < NUM_LEDS; i++) { //9948
-    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
-  }
-}
-
-void juggle() {
-  // eight colored dots, weaving in and out of sync with each other
-  fadeToBlackBy( leds, NUM_LEDS, 20);
-  byte dothue = 0;
-  for( int i = 0; i < 8; i++) {
-    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
 }
